75d2b871da143d9d538ecc2680bfebd3cfae069f
sreegayathridadireddy <124038460+sreegayathridadireddy@users.noreply.github.com>
2023-08-25 22:42:00 +0530

    Add files via upload

---
+++ stockk_dealer.java
@@ -0,0 +1,152 @@
+
+
+/**
+ * Class central_stock_system
+ */
+public class central_stock_system {
+
+  //
+  // Fields
+  //
+
+  private void store_stock_details;
+  
+  //
+  // Constructors
+  //
+  public central_stock_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_stock_details
+   * @param newVar the new value of store_stock_details
+   */
+  private void setStore_stock_details (void newVar) {
+    store_stock_details = newVar;
+  }
+
+  /**
+   * Get the value of store_stock_details
+   * @return the value of store_stock_details
+   */
+  private void getStore_stock_details () {
+    return store_stock_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void printbill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class stockk_dealer
+ */
+public class stockk_dealer {
+
+  //
+  // Fields
+  //
+
+  private void take_order;
+  private void enter_order;
+  private void verify_order;
+  
+  //
+  // Constructors
+  //
+  public stockk_dealer () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of take_order
+   * @param newVar the new value of take_order
+   */
+  private void setTake_order (void newVar) {
+    take_order = newVar;
+  }
+
+  /**
+   * Get the value of take_order
+   * @return the value of take_order
+   */
+  private void getTake_order () {
+    return take_order;
+  }
+
+  /**
+   * Set the value of enter_order
+   * @param newVar the new value of enter_order
+   */
+  private void setEnter_order (void newVar) {
+    enter_order = newVar;
+  }
+
+  /**
+   * Get the value of enter_order
+   * @return the value of enter_order
+   */
+  private void getEnter_order () {
+    return enter_order;
+  }
+
+  /**
+   * Set the value of verify_order
+   * @param newVar the new value of verify_order
+   */
+  private void setVerify_order (void newVar) {
+    verify_order = newVar;
+  }
+
+  /**
+   * Get the value of verify_order
+   * @return the value of verify_order
+   */
+  private void getVerify_order () {
+    return verify_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void deliver_item()
+  {
+  }
+
+
+}
