c306a99371cfbbae78288a3157a180bd372bca71
sreegayathridadireddy <124038460+sreegayathridadireddy@users.noreply.github.com>
2023-08-25 22:41:01 +0530

    Add files via upload

---
+++ custo.java
@@ -0,0 +1,118 @@
+
+
+/**
+ * Class central_stock_system
+ */
+public class central_stock_system {
+
+  //
+  // Fields
+  //
+
+  private void store_stock_details;
+  
+  //
+  // Constructors
+  //
+  public central_stock_system () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of store_stock_details
+   * @param newVar the new value of store_stock_details
+   */
+  private void setStore_stock_details (void newVar) {
+    store_stock_details = newVar;
+  }
+
+  /**
+   * Get the value of store_stock_details
+   * @return the value of store_stock_details
+   */
+  private void getStore_stock_details () {
+    return store_stock_details;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void printbill()
+  {
+  }
+
+
+  /**
+   */
+  public void delivery_product()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class custo
+ */
+public class custo {
+
+  //
+  // Fields
+  //
+
+  private void place_order;
+  
+  //
+  // Constructors
+  //
+  public custo () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of place_order
+   * @param newVar the new value of place_order
+   */
+  private void setPlace_order (void newVar) {
+    place_order = newVar;
+  }
+
+  /**
+   * Get the value of place_order
+   * @return the value of place_order
+   */
+  private void getPlace_order () {
+    return place_order;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void payment()
+  {
+  }
+
+
+}
