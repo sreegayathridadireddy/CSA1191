a840bb2ff38b4b76d2faeb11503b797c56f1f90b
sreegayathridadireddy <124038460+sreegayathridadireddy@users.noreply.github.com>
2023-08-28 10:15:10 +0530

    Add files via upload

---
+++ e_book_management_class.java
@@ -0,0 +1,173 @@
+
+
+/**
+ * Class admin
+ */
+public class admin {
+
+  //
+  // Fields
+  //
+
+  private void students_details;
+  private void book_list;
+  
+  //
+  // Constructors
+  //
+  public admin () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of students_details
+   * @param newVar the new value of students_details
+   */
+  private void setStudents_details (void newVar) {
+    students_details = newVar;
+  }
+
+  /**
+   * Get the value of students_details
+   * @return the value of students_details
+   */
+  private void getStudents_details () {
+    return students_details;
+  }
+
+  /**
+   * Set the value of book_list
+   * @param newVar the new value of book_list
+   */
+  private void setBook_list (void newVar) {
+    book_list = newVar;
+  }
+
+  /**
+   * Get the value of book_list
+   * @return the value of book_list
+   */
+  private void getBook_list () {
+    return book_list;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void verify_student_id()
+  {
+  }
+
+
+  /**
+   */
+  public void check_for_availability()
+  {
+  }
+
+
+  /**
+   */
+  public void issue_books()
+  {
+  }
+
+
+  /**
+   */
+  public void order_for_new_author()
+  {
+  }
+
+
+  /**
+   */
+  public void maintain_student_detail()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class e_book_management_class
+ */
+public class e_book_management_class {
+
+  //
+  // Fields
+  //
+
+  private void enter_id;
+  private void verify_user;
+  
+  //
+  // Constructors
+  //
+  public e_book_management_class () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of enter_id
+   * @param newVar the new value of enter_id
+   */
+  private void setEnter_id (void newVar) {
+    enter_id = newVar;
+  }
+
+  /**
+   * Get the value of enter_id
+   * @return the value of enter_id
+   */
+  private void getEnter_id () {
+    return enter_id;
+  }
+
+  /**
+   * Set the value of verify_user
+   * @param newVar the new value of verify_user
+   */
+  private void setVerify_user (void newVar) {
+    verify_user = newVar;
+  }
+
+  /**
+   * Get the value of verify_user
+   * @return the value of verify_user
+   */
+  private void getVerify_user () {
+    return verify_user;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void check_avalibility()
+  {
+  }
+
+
+}
