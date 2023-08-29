bc332195149f38ef1c15ad9827eda6aabe56a0dd
sreegayathridadireddy <124038460+sreegayathridadireddy@users.noreply.github.com>
2023-08-29 09:32:13 +0530

    Add files via upload

---
+++ Central_Management_System.java
@@ -0,0 +1,103 @@
+
+
+/**
+ * Class Central_Management_System
+ */
+public class Central_Management_System {
+
+  //
+  // Fields
+  //
+
+  private void Member_Details;
+  private void Function_Details;
+  private void Details_of_the_allocation;
+  
+  //
+  // Constructors
+  //
+  public Central_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Member_Details
+   * @param newVar the new value of Member_Details
+   */
+  private void setMember_Details (void newVar) {
+    Member_Details = newVar;
+  }
+
+  /**
+   * Get the value of Member_Details
+   * @return the value of Member_Details
+   */
+  private void getMember_Details () {
+    return Member_Details;
+  }
+
+  /**
+   * Set the value of Function_Details
+   * @param newVar the new value of Function_Details
+   */
+  private void setFunction_Details (void newVar) {
+    Function_Details = newVar;
+  }
+
+  /**
+   * Get the value of Function_Details
+   * @return the value of Function_Details
+   */
+  private void getFunction_Details () {
+    return Function_Details;
+  }
+
+  /**
+   * Set the value of Details_of_the_allocation
+   * @param newVar the new value of Details_of_the_allocation
+   */
+  private void setDetails_of_the_allocation (void newVar) {
+    Details_of_the_allocation = newVar;
+  }
+
+  /**
+   * Get the value of Details_of_the_allocation
+   * @return the value of Details_of_the_allocation
+   */
+  private void getDetails_of_the_allocation () {
+    return Details_of_the_allocation;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void storting()
+  {
+  }
+
+
+  /**
+   */
+  public void Updating_Details()
+  {
+  }
+
+
+  /**
+   */
+  public void Central_Management_System()
+  {
+  }
+
+
+}
---
+++ member.java
@@ -0,0 +1,207 @@
+
+
+/**
+ * Class Central_Management_System
+ */
+public class Central_Management_System {
+
+  //
+  // Fields
+  //
+
+  private void Member_Details;
+  private void Function_Details;
+  private void Details_of_the_allocation;
+  
+  //
+  // Constructors
+  //
+  public Central_Management_System () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of Member_Details
+   * @param newVar the new value of Member_Details
+   */
+  private void setMember_Details (void newVar) {
+    Member_Details = newVar;
+  }
+
+  /**
+   * Get the value of Member_Details
+   * @return the value of Member_Details
+   */
+  private void getMember_Details () {
+    return Member_Details;
+  }
+
+  /**
+   * Set the value of Function_Details
+   * @param newVar the new value of Function_Details
+   */
+  private void setFunction_Details (void newVar) {
+    Function_Details = newVar;
+  }
+
+  /**
+   * Get the value of Function_Details
+   * @return the value of Function_Details
+   */
+  private void getFunction_Details () {
+    return Function_Details;
+  }
+
+  /**
+   * Set the value of Details_of_the_allocation
+   * @param newVar the new value of Details_of_the_allocation
+   */
+  private void setDetails_of_the_allocation (void newVar) {
+    Details_of_the_allocation = newVar;
+  }
+
+  /**
+   * Get the value of Details_of_the_allocation
+   * @return the value of Details_of_the_allocation
+   */
+  private void getDetails_of_the_allocation () {
+    return Details_of_the_allocation;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void storting()
+  {
+  }
+
+
+  /**
+   */
+  public void Updating_Details()
+  {
+  }
+
+
+  /**
+   */
+  public void Central_Management_System()
+  {
+  }
+
+
+}
+
+
+
+/**
+ * Class member
+ */
+public class member {
+
+  //
+  // Fields
+  //
+
+  private void name;
+  private void id;
+  private void proof;
+  
+  //
+  // Constructors
+  //
+  public member () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of name
+   * @param newVar the new value of name
+   */
+  private void setName (void newVar) {
+    name = newVar;
+  }
+
+  /**
+   * Get the value of name
+   * @return the value of name
+   */
+  private void getName () {
+    return name;
+  }
+
+  /**
+   * Set the value of id
+   * @param newVar the new value of id
+   */
+  private void setId (void newVar) {
+    id = newVar;
+  }
+
+  /**
+   * Get the value of id
+   * @return the value of id
+   */
+  private void getId () {
+    return id;
+  }
+
+  /**
+   * Set the value of proof
+   * @param newVar the new value of proof
+   */
+  private void setProof (void newVar) {
+    proof = newVar;
+  }
+
+  /**
+   * Get the value of proof
+   * @return the value of proof
+   */
+  private void getProof () {
+    return proof;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void presenting_the_paper()
+  {
+  }
+
+
+  /**
+   */
+  public void winning_the_prize()
+  {
+  }
+
+
+  /**
+   */
+  public void member()
+  {
+  }
+
+
+}
